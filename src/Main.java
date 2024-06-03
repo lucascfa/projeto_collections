64bb87ef82b1b7653588fe56269788279149e90c
lucascfa <lucas.carvalhu.ferreira@gamil.com>
2024-06-02 21:45:12 -0300

    Finalizando módulo de collections

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 
 import Map.AgendaDeContato;
+import Map.AgendaEventos.AgendaEvento;
 import Map.EstoqueProdutos;
 import Produtos.CadastroProdutos;
 import Produtos.Produto;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        AgendaEvento agenda = new AgendaEvento();
+        agenda.adicionarEvento(LocalDate.of(2024, Month.JUNE,01),"Evento 1", "Atração 1");
+        agenda.adicionarEvento(LocalDate.of(2024, Month.JULY,24),"Evento 2", "Atração 2");
+        agenda.adicionarEvento(LocalDate.of(2024, Month.JUNE,26),"Evento 3", "Atração 3");
+        agenda.adicionarEvento(LocalDate.of(2024, Month.FEBRUARY,10),"Evento 4", "Atração 4");
+        agenda.adicionarEvento(LocalDate.of(2024, Month.OCTOBER,10),"Evento 5", "Atração 5");
 
+        agenda.exibirAgenda();
+
+        agenda.obterProximoEvento();
 
 
 
--- src/Map/AgendaEventos/AgendaEvento.java
+++ src/Map/AgendaEventos/AgendaEvento.java
@@ -1,8 +1,7 @@
 package Map.AgendaEventos;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AgendaEvento {
     private Map<LocalDate, Evento> agendaEvento;
@@ -13,6 +12,22 @@ public class AgendaEvento {
 
     public void adicionarEvento(LocalDate data, String nome, String atracao){
         agendaEvento.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> agendaOrdenadaPorData = new TreeMap<>(agendaEvento);
+        System.out.println(agendaOrdenadaPorData);
+
+    }
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosPorData = new TreeMap<>(agendaEvento);
+        for(Map.Entry<LocalDate, Evento> entry : eventosPorData.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                System.out.println("Próximo evento será " + entry.getValue() + " na data " + entry.getKey());
+                break;
+            }
+        }
     }
 
 }
