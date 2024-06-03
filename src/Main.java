a468fca8c26b6fc435849ce3134bd2698e416148
lucascfa <lucas.carvalhu.ferreira@gamil.com>
2024-06-02 21:17:07 -0300

    criado metodos da agendaEvento

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,7 @@ import Produtos.Produto;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Produto p1 =  new Produto("Produto 1", 23.90, 200, 1111);
-        Produto p2 =  new Produto("Produto 2", 11.90, 50, 1112);
-        Produto p3 =  new Produto("Produto 3", 15.90, 120, 1113);
-        Produto p4 =  new Produto("Produto 4", 32.90, 21, 1114);
-        Produto p5 =  new Produto("Produto 5", 50.90, 43, 1115);
 
-        EstoqueProdutos estoque = new EstoqueProdutos();
-
-        estoque.adicionarProduto(1111L, p1);
-        estoque.adicionarProduto(1112L, p2);
-        estoque.adicionarProduto(1113L, p3);
-        estoque.adicionarProduto(1114L, p4);
-        estoque.adicionarProduto(1115L, p5);
-
-        estoque.exibirProdutos();
-
-        System.out.println(estoque.calcularValorTotal());
-
-        System.out.println(estoque.obterProdutoMaisCaro());
 
 
 
--- src/Map/AgendaEventos/AgendaEvento.java
+++ src/Map/AgendaEventos/AgendaEvento.java
@@ -1,9 +1,18 @@
 package Map.AgendaEventos;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 public class AgendaEvento {
-    private Map<> agendaEvento;
+    private Map<LocalDate, Evento> agendaEvento;
 
+    public AgendaEvento() {
+        this.agendaEvento = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        agendaEvento.put(data, new Evento(nome, atracao));
+    }
 
 }
